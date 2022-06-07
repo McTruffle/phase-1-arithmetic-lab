0ac07677af37d11feade5e4d055013343e1cc713
nathank <iridescentfate@gmail.com>
2022-06-07 11:20:27 -0700

    commit tree 89a12186c973751e7d8e14d30f323352a196afcf

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+
+let random = Math.ceil(Math.random());
+
+
+let num3 = 16;
+let num4 = 4;
+let mod = num4 % num3;
+
+
+
+let max = Math.max(5, 16, 11,20);
